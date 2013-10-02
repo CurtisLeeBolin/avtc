a8d37f3418eebd84799304b7d14d5fbe9ff11a4e
Lee <curtlee2002@gmail.com>
2013-10-01 19:14:06 -0500

    Lots of Updates
    high profile
    check for vorbis audio
    new bit rate formula
    added 5.1(side) audio
    a lot of cleanup

--- avtc.py
+++ avtc.py
@@ -35,7 +35,9 @@ audioBitrateDict = {
 	'2' : 112,
 	'2 channels' : 112,
 	'5.1' : 276,
+	'5.1(side)' : 276,
 	'7.1' : 640}
+bitRateVary = 512.0
 
 def checkFileType(fileExtension):
 	fileExtension = fileExtension[1:].lower()
@@ -57,7 +59,7 @@ def printLog(s):
 		f.write(s + '\n')
 	print(s)
 
-def main():
+if __name__ == '__main__':
 	for dir in [inputDir, outputDir]:
 		if not os.path.exists(dir):
 			os.mkdir(dir, 0o0755)
@@ -68,51 +70,49 @@ def main():
 		if os.path.isfile(f):
 			fileName, fileExtension = os.path.splitext(f)
 			if checkFileType(fileExtension):
-				inputFile  = inputDir + '/' + f
-				outputFile = outputDir + '/' + fileName + '.mkv'
-				outputFilePart = outputFile + '.part'
+				inputFile  = '{}/{}'.format(inputDir, f)
+				outputFile = '{}/{}.mkv'.format(outputDir, fileName)
+				outputFilePart = '{}.part'.format(outputFile)
 				os.rename(f, inputFile)
+
 				printLog('{} Cropdetect started on {}'.format(time.strftime('%X'), fileName.__repr__()))
 				timeStarted = int(time.time())
-				try:
-					args = shlex.split('avconv -i ' + inputFile.__repr__() + ' -ss 00:02:00 -t 48 -vf cropdetect -an -sn -f rawvideo -y /dev/null')
-					stderrData = runSubprocess(args)
-					timeCompletedCrop = int(time.time()) - timeStarted
-					printLog('{} Cropdetect completed in {}'.format(time.strftime('%X'), datetime.timedelta(seconds=timeCompletedCrop)))
-					with open(inputFile + '.crop', 'w') as f:
-						f.write(stderrData)
-					durationLine = stderrData.split('Duration: ')[-1]
-					duration = durationLine.split(',')[0]
-					printLog('         Video Duration: {}'.format(duration))
-					cropLine = stderrData.split('cropdetect')[-1]
-					cropLine = cropLine.split('\n')[0]
-					crop = cropLine.split('crop')[1]
-				except:
-					args = shlex.split('avconv -i ' + inputFile.__repr__() + ' -ss 00:00:03 -t 5 -vf cropdetect -an -sn -f rawvideo -y /dev/null')
-					stderrData = runSubprocess(args)
-					timeCompletedCrop = int(time.time()) - timeStarted
-					printLog('{} Cropdetect completed in {}'.format(time.strftime('%X'), datetime.timedelta(seconds=timeCompletedCrop)))
-					with open(inputFile + '.crop', 'w') as f:
-						f.write(stderrData)
-					durationLine = stderrData.split('Duration: ')[-1]
-					duration = durationLine.split(',')[0]
-					printLog('         Video Duration: {}'.format(duration))
-					cropLine = stderrData.split('cropdetect')[-1]
-					cropLine = cropLine.split('\n')[0]
-					crop = cropLine.split('crop')[1]
-				w = cropLine.split('w:')[1]
-				w = int(w.split(' ')[0])
-				h = cropLine.split('h:')[1]
-				h = int(h.split(' ')[0])
-				videoBitrate = (((w * h) ** 0.5) * 25 / 18).__round__()
+
+				args = shlex.split('ffmpeg -i {}'.format(inputFile.__repr__()))
+				stderrData = runSubprocess(args)
+				duration = re.findall('Duration: (.*?),', stderrData)[-1]
+				audioCodec = re.findall('Audio: (.*?),', stderrData)[-1]
+				durationList = duration.split(':')
+				durationSec = 60 * 60 * int(durationList[0]) + 60 * int(durationList[1]) + float(durationList[2])
+				cropDetectStart =  str(datetime.timedelta(seconds=(durationSec / 10)))
+				cropDetectDuration =  str(datetime.timedelta(seconds=(durationSec / 100)))
+				args = shlex.split('ffmpeg -i {} -ss {} -t {} -filter:v cropdetect -an -sn -f rawvideo -y /dev/null'.format(inputFile.__repr__(), cropDetectStart, cropDetectDuration))
+				stderrData = runSubprocess(args)
+
+				timeCompletedCrop = int(time.time()) - timeStarted
+				printLog('{} Cropdetect completed in {}'.format(time.strftime('%X'), datetime.timedelta(seconds=timeCompletedCrop)))
+
+				with open(inputFile + '.crop', 'w') as f:
+					f.write(stderrData)
+				printLog('         Video Duration: {}'.format(duration))
+				crop = re.findall('crop=(.*?)\n', stderrData)[-1]
+				cropList = crop.split(':')
+				w = int(cropList[0])
+				h = int(cropList[1])
+
+				resolution = w * h / 1024.0
+				videoBitrate = ( resolution * ( 2537.0**(1.0/2.5) / resolution**(1.0/2.5) ) ).__round__()
+				videoBitrateMax = ( videoBitrate + bitRateVary ).__round__()
+				if ( videoBitrate > bitRateVary ):
+					videoBitrateMin = ( videoBitrate - bitRateVary ).__round__()
+				else:
+					videoBitrateMin = 0
 				printLog('         Video output {}x{} at {}kb/s'.format(w, h, videoBitrate))
-				audioLine = stderrData.split('Audio: ')[-1]
-				audioLine = stderrData.split('Hz, ')[-1]
-				audioCh = audioLine.split(',')[0]
+
+				audioCh = re.findall(' Hz, (.*?),', stderrData)[-1]
+
 				audioBitrate = audioBitrateDict.get(audioCh,'unknown')
 				if duration != 'N/A':
-					timeList = list(map(int, re.split(r"[:.]", duration)))
-					durationSec = (timeList[0] * 3600. + timeList[1] * 60. + timeList[2] + timeList[3] / 1000.)
 					if audioBitrate != 'unknown':
 						printLog('         Audio output {} at {}kb/s'.format(audioCh, audioBitrate))
 						printLog('         Estimated output size of {}MB at {}kb/s'.format((((videoBitrate + audioBitrate) * durationSec)/(1024 * 8)).__round__(), (videoBitrate + audioBitrate)))
@@ -123,7 +123,7 @@ def main():
 					printLog('         Estimated file size can\'t be calculated since the duration is unknown.')
 				timeStartPass1 = int(time.time())
 				printLog('         Pass1 Started')
-				args = shlex.split('avconv -i ' + inputFile.__repr__() + ' -pass 1 -passlogfile ' + inputDir + '/0pass -vf crop' + crop + ' -c:v libx264 -preset veryslow -b:v ' +  videoBitrate.__str__() + 'k -an -sn -f rawvideo -y /dev/null')
+				args = shlex.split('ffmpeg -i {} -pass 1 -passlogfile {}/0pass -vf crop={} -c:v libx264 -preset veryslow -profile high -b:v {}k -maxrate {}k -minrate {}k -an -sn -f rawvideo -y /dev/null'.format(inputFile.__repr__(), inputDir, crop, videoBitrate.__str__(), videoBitrateMax.__str__(), videoBitrateMin.__str__()))
 				stderrData = runSubprocess(args)
 				timeCompletedPass1 = int(time.time()) - timeStartPass1
 				printLog('{} Pass1 completed in {}'.format(time.strftime('%X'), datetime.timedelta(seconds=timeCompletedPass1)))
@@ -131,7 +131,10 @@ def main():
 					f.write(stderrData)
 				timeStartPass2 = int(time.time())
 				printLog('         Pass2 Started')
-				args = shlex.split('avconv -i ' + inputFile.__repr__() + ' -pass 2 -passlogfile ' + inputDir + '/0pass -vf crop' + crop + ' -c:v libx264 -preset veryslow -b:v ' +  videoBitrate.__str__() + 'k -c:a libvorbis -q:a 3 -sn -f matroska -metadata title="' + fileName + '" ' + outputFilePart.__repr__())
+				if 'vorbis' in audioCodec:
+					args = shlex.split('ffmpeg -i {} -pass 2 -passlogfile {}/0pass -vf crop={} -c:v libx264 -preset veryslow -profile high -b:v {}k -maxrate {}k -minrate {}k -c:a copy -c:s copy -f matroska -metadata title="{}" {}'.format(inputFile.__repr__(), inputDir, crop, videoBitrate.__str__(), videoBitrateMax.__str__(), videoBitrateMin.__str__(), fileName, outputFilePart.__repr__()))
+				else:
+					args = shlex.split('ffmpeg -i {} -pass 2 -passlogfile {}/0pass -vf crop={} -c:v libx264 -preset veryslow -profile high -b:v {}k -maxrate {}k -minrate {}k -c:a libvorbis -q:a 3 -c:s copy -f matroska -metadata title="{}" {}'.format(inputFile.__repr__(), inputDir, crop, videoBitrate.__str__(), videoBitrateMax.__str__(), videoBitrateMin.__str__(), fileName, outputFilePart.__repr__()))
 				stderrData = runSubprocess(args)
 				timeCompletedPass2 = int(time.time()) - timeStartPass2
 				printLog('{} Pass2 completed in {}'.format(time.strftime('%X'), datetime.timedelta(seconds=timeCompletedPass2)))
@@ -142,7 +145,3 @@ def main():
 				timeJobSeconds = timeCompleted - timeStarted
 				timeJob = str(datetime.timedelta(seconds=timeJobSeconds))
 				printLog('         Completed transcoding in {}\n'.format(timeJob))
-	return 0
-
-if __name__ == '__main__':
-	main()
