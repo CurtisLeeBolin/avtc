82dd2d17ba2a1a4b5ba42522bca839641dd3f2e0
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2013-11-23 22:29:14 -0600

    Changed Email Address

--- avtc.py
+++ avtc.py
@@ -3,7 +3,7 @@
 #
 #  avtc.py
 #
-#  Copyright 2013 Curtis Lee Bolin <curtlee2002@gmail.com>
+#  Copyright 2013 Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
